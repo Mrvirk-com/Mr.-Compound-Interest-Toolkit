98e79d0f66ab8b8eee4d685e7011ef0620bf8103
Virksaabnavjot <virksaabnavjot@gmail.com>
2020-03-29 21:38:09 +0100

    Implemented Basic UI, imported couple boilerplate Google Charts (need change soon)
    What's next: Work to perform compounding calculations and pass them to google charts
    
    Oppurtunity: The opppurtunity here is amazing, I have couple very amazing ideas in mind to build the best in market compound interset calculator and visualisation using google charts
    
    - using charts toolbar feature can allow the user to export their calculations, data and charts: https://developers.google.com/chart/interactive/docs/gallery/toolbar
    
    - using dashboard allow user to interact with forms: https://developers.google.com/chart/interactive/docs/gallery/controls

--- logic.js
+++ logic.js
@@ -1,62 +1,159 @@
-calculate_savings_and_discounted_price();
-calculate_savings_on_fixed_price_off();
-discount_form.oninput = function(event) {
+compoundingCalculator();
+compoundingChart();
+calculationsTable();
+compounding_vs_simple_interest_chart();
+yearly_compound_growth_deposits_and_simple_intrest_corelation_chart();
+compounding_form.oninput = function(event) {
   event.preventDefault();
-  calculate_savings_and_discounted_price();
+  compounding_calculator();
 };
 
-fixed_off_form.oninput = function(event) {
-  event.preventDefault();
-  calculate_savings_on_fixed_price_off();
-};
 
-/*
-What is the 33% of 67 ?
-Let, savings= ?, discount_percent= 33, product_price= 67
-savings= (product_price * discount_percent)/100
-*/
-function calculate_savings_and_discounted_price() {
-  const form = document.forms.discount_form;
-  let discount_percent= form.elements.discount_percent.value;
-  let product_price = form.elements.product_price.value;
-  if (discount_percent== null || discount_percent== "") {
-    discount_percent= 0;
-  }
-  if (product_price== null || product_price== "") {
-    product_price= 0;
-  }
-  let savings= (discount_percent* product_price) / 100;
-  form.elements.savings.value = savings.toFixed(2);
+function compoundingCalculator() {
+  const form = document.forms.compounding_form;
+  let principal = form.elements.principal.value;
 
-  let discounted_price = product_price - savings;
-  form.elements.discounted_price.value = discounted_price;
+}
 
-  if (savings< 0) {
-    form.elements.savings.className = "result-negative"
-  } else {
-    form.elements.savings.className = "result"
+function compoundingChart() {
+
+  // Load the Visualization API and the corechart package.
+  google.charts.load('current', {
+    'packages': ['corechart']
+  });
+
+  // Set a callback to run when the Google Visualization API is loaded.
+  google.charts.setOnLoadCallback(drawChart);
+
+  // Callback that creates and populates a data table,
+  // instantiates the pie chart, passes in the data and
+  // draws it.
+  function drawChart() {
+
+    // Create the data table.
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Topping');
+    data.addColumn('number', 'Slices');
+    data.addRows([
+      ['Mushrooms', 3],
+      ['Onions', 1],
+      ['Olives', 1],
+      ['Zucchini', 1],
+      ['Pepperoni', 2]
+    ]);
+
+    // Set chart options
+    var options = {
+      'title': 'How Much Pizza I Ate Last Night',
+      'width': 400,
+      'height': 300
+    };
+
+    // Instantiate and draw our chart, passing in some options.
+    var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+    chart.draw(data, options);
   }
 }
 
-function calculate_savings_on_fixed_price_off() {
-  const form = document.forms.fixed_off_form;
-  let fixed_off= form.elements.fixed_off.value;
-  let product_price = form.elements.product_price.value;
-  if (fixed_off== null || fixed_off== "") {
-    fixed_off= 0;
-  }
-  if (product_price== null || product_price== "") {
-    product_price= 0;
-  }
-  let savings_percent= (fixed_off / product_price) * 100;
-  form.elements.savings_percent.value = savings_percent.toFixed(2)+" %";
+function compounding_vs_simple_interest_chart() {
+  google.charts.load('current', {
+    'packages': ['corechart']
+  });
+  google.charts.setOnLoadCallback(drawChart);
 
-  let discounted_price = product_price - fixed_off;
-  form.elements.discounted_price.value = discounted_price;
+  function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+      ['Year', 'Sales', 'Expenses'],
+      ['2013', 1000, 400],
+      ['2014', 1170, 460],
+      ['2015', 660, 1120],
+      ['2016', 1030, 540]
+    ]);
 
-  if (savings_percent< 0) {
-    form.elements.savings_percent.className = "result-negative"
-  } else {
-    form.elements.savings_percent.className = "result"
+    var options = {
+      title: 'Company Performance',
+      hAxis: {
+        title: 'Year',
+        titleTextStyle: {
+          color: '#333'
+        }
+      },
+      vAxis: {
+        minValue: 0
+      }
+    };
+
+    var chart = new google.visualization.AreaChart(document.getElementById('compound_vs_simple_div'));
+    chart.draw(data, options);
+  }
+}
+
+function yearly_compound_growth_deposits_and_simple_intrest_corelation_chart() {
+  google.charts.load('current', {
+    'packages': ['bar']
+  });
+  google.charts.setOnLoadCallback(drawChart);
+
+  function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+      ['Year', 'Sales', 'Expenses', 'Profit'],
+      ['2014', 1000, 400, 200],
+      ['2015', 1170, 460, 250],
+      ['2016', 660, 1120, 300],
+      ['2017', 1030, 540, 350]
+    ]);
+
+    var options = {
+      chart: {
+        title: 'Company Performance',
+        subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+      },
+      bars: 'horizontal' // Required for Material Bar Charts.
+    };
+
+    var chart = new google.charts.Bar(document.getElementById('barchart_material'));
+
+    chart.draw(data, google.charts.Bar.convertOptions(options));
+  }
+}
+
+
+function calculationsTable() {
+  google.charts.load('current', {
+    'packages': ['table']
+  });
+  google.charts.setOnLoadCallback(drawTable);
+
+  function drawTable() {
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Name');
+    data.addColumn('number', 'Salary');
+    data.addColumn('boolean', 'Full Time Employee');
+    data.addRows([
+      ['Mike', {
+        v: 10000,
+        f: '$10,000'
+      }, true],
+      ['Jim', {
+        v: 8000,
+        f: '$8,000'
+      }, false],
+      ['Alice', {
+        v: 12500,
+        f: '$12,500'
+      }, true],
+      ['Bob', {
+        v: 7000,
+        f: '$7,000'
+      }, true]
+    ]);
+
+    var table = new google.visualization.Table(document.getElementById('table_div'));
+
+    table.draw(data, {
+      showRowNumber: true,
+      width: '100%',
+      height: '100%'
+    });
   }
 }
