099066e1f72b72328a44834bcb6cc0637a2e024e
Navjot Singh Virk <virksaabnavjot@gmail.com>
2020-06-21 01:14:32 +0100

    Working Compounding Calculator
    What's next:
    1- Build functionality to a deposits and set frequency
    2- Store Data by the year so it can be used for building charts later, plan is to have an array storing [year number, principal, futurevalue] example:
    let dataArray = [[year1, 1000, 1150] , [year2, 1000, 1322.50], [...., ...., ....]];

--- logic.js
+++ logic.js
@@ -5,13 +5,41 @@ compounding_vs_simple_interest_chart();
 yearly_compound_growth_deposits_and_simple_intrest_corelation_chart();
 compounding_form.oninput = function(event) {
   event.preventDefault();
-  compounding_calculator();
+  compoundingCalculator();
 };
 
 
 function compoundingCalculator() {
-  const form = document.forms.compounding_form;
-  let principal = form.elements.principal.value;
+  //inputs
+  const form = document.forms.compounding_form.elements;
+  let principal = form.principal.value;
+  let interest_rate = form.interest_rate.value;
+  let compounding_frequency = form.compounding_frequency.value;
+  let growth_period = form.growth_period.value;
+  let deposit = form.deposit.value;
+  let deposit_frequency = form.deposit_frequency.value;
+
+  //outputs
+  let your_contributions = form.your_contributions;
+  let future_value = form.future_value;
+  let interest_amount = form.interest_amount;
+  let growth_percent = form.growth_percent;
+  let profit = form.profit;
+  //calculations
+  // The equation is A = p * [[1 + (r/n)] ^ nt]
+  //compound_interest = (principal * Math.pow((1 + (interest_rate / (compounding_frequency * 100))), (compounding_frequency * growth_period)));
+  your_contributions.value = principal;
+
+  let compound_interest;
+  let i;
+  for (i = 1; i <= growth_period; i++) {
+    compound_interest = (principal * Math.pow((1 + (interest_rate / (compounding_frequency * 100))), (compounding_frequency * growth_period)));
+    console.log("Run = " + i);
+
+  }
+
+
+  future_value.value = compound_interest.toFixed(2);
 
 }
 
